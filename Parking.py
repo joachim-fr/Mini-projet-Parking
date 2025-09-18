e527a7e75dc23c3e4a9e7bfd182ec4a133d34eab
t350b616-create <t350.b616@gmail.com>
2025-09-18 11:13:48 +0200

    j'ai ajouté du code

---
+++ Parking.py
@@ -0,0 +1,9 @@
+class Parking:
+
+    def __init__(self, etage, place, abonnes, nom):
+        places_parking = {}
+        places_abos = {}
+        self.etage = etage
+        self.place = place
+        self.abonnes = abonnes
+        self.nom = nom
