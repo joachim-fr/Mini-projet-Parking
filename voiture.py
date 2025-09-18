48b269cd20c7c416a74113a4b608800e41f619ab
t350b616-create <t350.b616@gmail.com>
2025-09-18 11:02:33 +0200

    j'ai ajouté voiture.py

---
+++ voiture.py
@@ -0,0 +1,7 @@
+class Voiture:
+
+    def __init__(self, parking, imma, marque, proprietaire):
+        self.parking = parking
+        self.imma = imma
+        self.marque = marque
+        self.proprietaire = proprietaire
